1b02e4f8293fe7b31b7d91ca2270bbf0775a9df0
Nolan Stone <88562716+3xionDev@users.noreply.github.com>
2024-11-20 09:22:01 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const holder = document.getElementById('holder');
+const ctx = document.getElementById('ctx');
 
 function collatz() {
+  chart.destroy();
   let value = document.getElementById('inputBox').value;
+  let steps = [];
   let a = [];
   let i = 0;
   holder.innerHTML = "a = [";
@@ -10,21 +13,59 @@ function collatz() {
       if ((value % 2) == 0) {
         value *= 0.5;
         i++;
+        steps.push(i);
         holder.innerHTML += value;
         if (value != 1) {
           holder.innerHTML += ",";
         }
+        a.push(value);
       } else {
         value = (value * 3) + 1;
         i++;
+        steps.push(i);
         holder.innerHTML += value;
         if (value != 1) {
           holder.innerHTML += ",";
         }
+        a.push(value);
       }
     }
     holder.innerHTML += "] <br> b = [0..." + i + "]";
+
+    const chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: steps,
+        datasets: [{
+          label: 'Sequence Integers',
+          data: a,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            title: {
+              display: true,
+              text: "Numbers",
+              align: 'center',
+            },
+            beginAtZero: true,
+            suggestedMin: 0,
+            suggestedMax: value + 10
+          },
+          x: {
+            title: {
+              display: true,
+              text: 'Steps',
+              align: 'center',
+            }
+          }
+        }
+      }
+    });
   } catch (err) {
     holder.innerHTML = err;
   }
 }
+
